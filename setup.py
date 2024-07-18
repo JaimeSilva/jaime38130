83fd503fd2bd758e2c6e1421979b26af75bebf5f
Jaime <jaimedcsilva@hotmail.com>
2024-07-18 15:25:45 +0100

    Versão 0.6: Descrição das alterações feitas

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 from distutils.core import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'jaime38130',
-  packages = ['jaime38130'],
-  version = '0.5',
-  license='-',
-  description = 'Colors!',
-  author = 'Jaime Silva',
-  author_email = 'jaimedcsilva@hotmail.com',
-  url = 'https://github.com/JaimeSilva/jaime38130.git',
-  download_url = 'https://github.com/JaimeSilva/jaime38130/archive/refs/tags/v_05.tar.gz',
-  keywords = ['SOME', 'MEANINGFULL', 'COLORS'],
-  install_requires=[
-          'colorama',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.10',
-  ],
+    name='jaime38130',
+    packages=['jaime38130'],
+    version='0.6',
+    license='MIT',
+    description='I needed colors',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jaime Silva',
+    author_email='jaimedcsilva@hotmail.com',
+    url='https://github.com/JaimeSilva/jaime38130.git',
+    download_url='https://github.com/JaimeSilva/jaime38130/archive/refs/tags/v_06.tar.gz',
+    keywords=['SOME', 'MEANINGFULL', 'COLORS'],
+    install_requires=[
+        'colorama',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
